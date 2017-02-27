4d140098cf0c137230aedcbc6d6ca3d50148165e
AkshGautam <gaksh007@gmail.com>
2017-02-28 00:37:28 +0530

    Layout Improved

--- app/src/main/java/com/example/aksh/toptens/TrackAdapter.java
+++ app/src/main/java/com/example/aksh/toptens/TrackAdapter.java
@@ -1,7 +1,9 @@
 package com.example.aksh.toptens;
 
 import android.content.Context;
+import android.graphics.drawable.GradientDrawable;
 import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +11,9 @@ import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import java.util.List;
+
+import static com.example.aksh.toptens.R.id.current;
+import static com.example.aksh.toptens.R.id.previous;
 
 /**
  * Created by aksh on 21/2/17.
@@ -32,12 +37,29 @@ public class TrackAdapter extends ArrayAdapter<Track> {
         songTextView.setText(currentTrack.getTitle());
         TextView artistTextView=(TextView)listItemView.findViewById(R.id.artist);
         artistTextView.setText(currentTrack.getArtist());
-        TextView previousTextView=(TextView)listItemView.findViewById(R.id.previous);
+        TextView previousTextView=(TextView)listItemView.findViewById(previous);
         previousTextView.setText(currentTrack.getPrevious_ranking());
-       TextView currentTextView=(TextView)listItemView.findViewById(R.id.current);
+       TextView currentTextView=(TextView)listItemView.findViewById(current);
         currentTextView.setText(currentTrack.getCurrent_ranking());
-         return listItemView;
-
+        GradientDrawable current_Circle=(GradientDrawable)currentTextView.getBackground();
+        int Color = getColor(currentTrack.getPrevious_ranking(),currentTrack.getCurrent_ranking());
+        current_Circle.setColor(Color);
+        GradientDrawable Prev_Circle=(GradientDrawable)previousTextView.getBackground();
+        Prev_Circle.setColor(ContextCompat.getColor(getContext(),R.color.colorPrimary));
+        return listItemView;
+    }
+    private int getColor(String previous,String current) {
+        int ColorResourceId,p,c;
+        p=Integer.parseInt(previous);
+        c=Integer.parseInt(current);
+        if(p<c&&p!=0)
+            ColorResourceId=R.color.down;
+        else if(c==p||p==0)
+            ColorResourceId=R.color.colorPrimary;
+        else
+            ColorResourceId=R.color.up;
+        return ContextCompat.getColor(getContext(),ColorResourceId);
     }
 
 }
+
